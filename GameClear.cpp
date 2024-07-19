8cc6fe02dfaacc6c150809ada8fb052a419d950d
武琉 本間 <230492@jc-21.jp>
2024-07-19 12:39:24 +0900

    ファイルにまとめた

---
+++ GameClear.cpp
@@ -0,0 +1,21 @@
+#include "GameClear.h"
+
+GameClear::GameClear(GameObject* parent)
+{
+}
+
+void GameClear::Initialize()
+{
+}
+
+void GameClear::Update()
+{
+}
+
+void GameClear::Draw()
+{
+}
+
+void GameClear::Release()
+{
+}
---
+++ GameClear.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "GameObject.h"
+class GameClear :
+    public GameObject
+{
+public:
+	//コンストラクタ
+	//引数：parent  親オブジェクト（SceneManager）
+	GameClear(GameObject* parent);
+
+	//初期化
+	void Initialize() override;
+
+	//更新
+	void Update() override;
+
+	//描画
+	void Draw() override;
+
+	//開放
+	void Release() override;
+};
